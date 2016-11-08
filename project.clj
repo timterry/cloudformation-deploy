a6fa329d4789e1e4e6345ef8a38c3af4e859a2bf
tim.terry <tim.terry@agentsmutual.co.uk>
2016-11-08 22:58:17 +0000

    Added a description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject tterry/cloudformation-deploy "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject tterry/cloudformation-deploy "0.1.0"
+  :description "Deploys AWS cloudformation templates using stack updates"
+  :url "https://github.com/timterry/cloudformation-deploy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
